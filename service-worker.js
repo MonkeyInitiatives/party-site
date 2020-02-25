2999121295376a1e0115226062c24c93d194812b
Thomas Woods <twoods@MacBook-Pro.local>
2020-02-25 18:18:10 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/party-site/precache-manifest.a89dc74eca520ab171e6755a91c3e605.js"
+  "/party-site/precache-manifest.e6c92be30a01f3cc750f18955cb2dbaa.js"
 );
 
 workbox.clientsClaim();
