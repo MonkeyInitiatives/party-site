57ab0ea8fb33d2a718ab63c991789d7fbc3ceb4d
Thomas Woods <twoods@MacBook-Pro.local>
2020-02-25 18:55:09 -0500

    Updates

--- precache-manifest.e547f7a2c106499498f18245e84f7961.js
+++ precache-manifest.e547f7a2c106499498f18245e84f7961.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/party-site/static/js/runtime~main.40ac4f9f.js"
   },
   {
-    "revision": "9c44278b51bdd22553ee",
-    "url": "/party-site/static/js/main.a2c77e35.chunk.js"
+    "revision": "f1e9911d2a1fa10492c9",
+    "url": "/party-site/static/js/main.ac61309e.chunk.js"
   },
   {
     "revision": "191ad65f4d155d8d51fc",
     "url": "/party-site/static/js/2.58f16dfd.chunk.js"
   },
   {
-    "revision": "9c44278b51bdd22553ee",
+    "revision": "f1e9911d2a1fa10492c9",
     "url": "/party-site/static/css/main.173d01e4.chunk.css"
   },
   {
-    "revision": "6c7156f038d97a98466ac71769903ee3",
+    "revision": "cf788da5a20c23278fc303d564e1aafa",
     "url": "/party-site/index.html"
   }
 ];
